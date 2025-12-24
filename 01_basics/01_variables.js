86c2a1fbf61b144c43c938dec369afd3fd715d3e
vijaygader94 <150414939+vijaygader94@users.noreply.github.com>
2025-12-24 10:53:48 +0000

    table form

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountid =144553
+let accountemail ="vijay.com"
+var accountPassword ="123456"
+accountCity = "jaipur"
+let accountstate;
+//acountid =2// not allowed
+
+accountemail ="vijgmail.com"
+accountpassword ="78787878"
+accountcity ="abohar"
+
+console.log(accountid);
+
+console.table([accountid,accountemail, accountpassword,accountcity,accountstate])
+/* var ko use nhi krenge kyuki y block scope and functional scope
+*/
